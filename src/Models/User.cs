d341bd6df0b6346346fa63bb22bce2f80a54b1b0
codercengiz <codercengiz@gmail.com>
2021-03-11 16:36:39 +0100

    Add models

---
+++ src/Models/User.cs
@@ -0,0 +1,27 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+using Newtonsoft.Json;
+
+
+namespace NoteApp_UserManagement_Api.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+
+        [BsonElement("Name")]
+        [JsonProperty("Name")]
+        public string UserName { get; set; }
+
+
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
